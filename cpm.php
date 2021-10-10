04f0c09df23b7c82ea9fde050be2cf3ca7f24028
Ameer Mousavi <xerxes1718@gmail.com>
2021-10-10 07:30:37 +0330

    update translation and desc

--- cpm.php
+++ cpm.php
@@ -15,12 +15,12 @@
  * @wordpress-plugin
  * Plugin Name:       Custom Payment
  * Plugin URI:        https://ameer.ir/cpm
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Enable users to pay a custom amount using BPM gateway
  * Version:           1.0.0
  * Author:            Ameer Mousavi
  * Author URI:        https://ameer.ir
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       cpm
  * Domain Path:       /languages
  */
